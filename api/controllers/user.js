88c6bf2f6d7be185342e42724df26bf54a1e7c31
Oseias Nogueira <oseias.n.m@gmail.com>
2023-05-21 03:22:41 -0300

    Construção de Controllers e Routes

---
+++ api/controllers/user.js
@@ -0,0 +1,11 @@
+import {db} from "../db.js";
+
+export const getUsers = (_, res) => {
+    const q = "SELECT * FROM usuários";
+
+    db.query(q, (error, data) => {
+        if(err) return res.json(err);
+
+        return res.status(200).json(data);
+    });
+};
